fd4ba3fb7d13d5873605f81d4c1899e512432da6
John <jjdesolutions@gmail.com>
2018-02-24 15:20:20 -0500

    added coprime module to Cipher

--- ciphers.py
+++ ciphers.py
@@ -1,3 +1,6 @@
+from math import gcd
+
+
 class Cipher:
     def encrypt(self):
         raise NotImplementedError()
@@ -5,5 +8,11 @@ class Cipher:
     def decrypt(self):
         raise NotImplementedError()
 
-
-
+    def coprime(self, a: int, b: int):
+        """
+        Returns True if 'a' is a coprime of 'b'
+        :param a: int
+        :param b: int
+        :return: Bool
+        """
+        return gcd(a, b) == 1
